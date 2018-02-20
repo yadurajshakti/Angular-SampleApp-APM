6d7d2c0cea62d9fc28ddd5fb7a780dcc2c1bb1e7
Aman@Aman-PC <yaduraj.shakti@gmail.com>
2018-02-21 04:01:29 +0530

    added App component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component} from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template: `<div>
-  <h1>{{pageTitle}}</h1>
+  template: `<div><h1>{{pageTitle}}</h1>
   <div>My First component</div>
   </div>`
 })
