2559c718372b198787e94e5f5de3d77a925002ed
yadurajshakti <yaduraj.shakti@gmail.com>
2018-02-22 04:30:39 +0530

    improved product component with interface, filter, pipes

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,16 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
     pageTitle: string = 'Product List';
-    imageWidth: number = 50;
+    imageWidth: number = 25;
     imageMargin: number = 2;
     showImage: boolean = false;
     errorMessage: string;
-    products: any[] =
+    _listFilter: string;
+
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value: string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    filteredProducts: IProduct[];
+
+    products: IProduct[] =
     [{
         "productId": 1,
         "productName": "Leaf Rake",
@@ -42,7 +55,22 @@ export class ProductListComponent {
         "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
     }];
 
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = 'cart';
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
     toggleImage(): void {
         this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        console.log('in OnInit');
     }
 }
