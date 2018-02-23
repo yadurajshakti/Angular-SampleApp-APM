dd2be03530df8139cf22e0b9fde4e9b8d54e0dda
yadurajshakti <yaduraj.shakti@gmail.com>
2018-02-24 04:50:22 +0530

    services and DI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { ProductService } from './products/product.service';
 
 @Component({
   selector: 'pm-root',
   template: `<div><h1>{{pageTitle}}</h1>
   <pm-products></pm-products>
-  </div>`
+  </div>`,
+
+  providers: [ProductService]
 })
 export class AppComponent {
-  pageTitle: string ='Acme Product Management';
+  pageTitle: string = 'Acme Product Management';
 }
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -23,41 +24,11 @@ export class ProductListComponent implements OnInit {
 
     filteredProducts: IProduct[];
 
-    products: IProduct[] =
-    [{
-        "productId": 1,
-        "productName": "Leaf Rake",
-        "productCode": "GDN-0011",
-        "releaseDate": "March 19, 2016",
-        "description": "Leaf rake with 48-inch wooden handle.",
-        "price": 19.95,
-        "starRating": 3.2,
-        "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Leaf-Rake.png"
-    },
-    {
-        "productId": 2,
-        "productName": "Garden Cart",
-        "productCode": "GDN-0023",
-        "releaseDate": "March 18, 2016",
-        "description": "15 gallon capacity rolling garden cart",
-        "price": 32.99,
-        "starRating": 4.2,
-        "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    },
-    {
-        "productId": 5,
-        "productName": "Hammer",
-        "productCode": "TBX-0048",
-        "releaseDate": "May 21, 2016",
-        "description": "Curved claw steel hammer",
-        "price": 8.9,
-        "starRating": 4.8,
-        "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
-    }];
+    products: IProduct[] = [];
 
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+
+    constructor(private _productService: ProductService) {
+
     }
 
     performFilter(filterBy: string): IProduct[] {
@@ -69,12 +40,13 @@ export class ProductListComponent implements OnInit {
     onRatingClicked(message: string): void {
         this.pageTitle = 'Product List: ' + message;
     }
-    
+
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
 
     ngOnInit(): void {
-        console.log('in OnInit');
+        this.products = this._productService.getProducts();
+        this.filteredProducts = this.products;
     }
 }
