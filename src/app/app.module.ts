ff212f50adb6d00b6ba17e69f629d9c37fb53701
yadurajshakti <yaduraj.shakti@gmail.com>
2018-02-24 13:28:01 +0530

    Http and exception handling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';  
+import { StarComponent } from './shared/star.component';
+
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
     StarComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -46,7 +46,12 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.products = this._productService.getProducts();
-        this.filteredProducts = this.products;
+        this._productService.getProducts()
+            .subscribe(products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error => this.errorMessage = <any>error);
+
     }
 }
